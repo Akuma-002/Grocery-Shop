8f00080c8b22109877f300ce42ae833dd75c49c0
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-11 16:15:14 +0530

    set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+// Import Bootstrap's CSS (if not done globally already)
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
-const App = () =>{
-  return(
+
+const App = () => {
+  return (
     <div>
+      <div className="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button className="btn btn-primary">Primary button</button>
+      </div>
     </div>
   )
 }
-
 
 export default App
