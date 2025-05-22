c059b53156e9585f187a84a4e454a85938550daf
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-22 19:42:04 +0530

    improvement

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,9 @@ function Navbar() {
   return (
 <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to="/">
+            <NavLink to="/" onClick={()=>{
+                setOpen(false);
+            }}>
                 <img className="h-9" src="./assets/logo.png" alt="dummyLogoColored" />
             </NavLink>
 
@@ -33,7 +35,7 @@ function Navbar() {
                     </svg>
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div onClick={()=>{navigate ("/cart")}} className="relative cursor-pointer">
                     <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
