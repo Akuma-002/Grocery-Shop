5e078141a8474b9aa49925211d50e2cca6f8f20d
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-22 11:20:41 +0530

    css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
+    <>
     <div>
+      <Navbar/>
       <div className="container py-4 px-3 mx-auto">
         <h1>Hello, Bootstrap and Vite!</h1>
         <button className="btn btn-primary">Primary button</button>
       </div>
     </div>
+      
+    </>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom' /* For routing */
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
+import './styles/custom.scss';
+
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
+  </StrictMode>,
 )
