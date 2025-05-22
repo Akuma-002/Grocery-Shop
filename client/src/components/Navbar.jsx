28c5ca976fe9554065091c9bb7abbe1a00a4249f
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-22 19:35:24 +0530

    Login & Logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Navigate, NavLink } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext';
 
 function Navbar() {
-    const [open, setOpen] = React.useState(false)
+    const [open, setOpen] = React.useState(false);
+    const {user, setUser, setShowUserLogin, navigate} = useAppContext();
+    const logout = async()=>{
+        setUser(null);
+        navigate('/')               
+    }
   return (
 <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
@@ -12,9 +18,12 @@ function Navbar() {
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
+                <NavLink to="/" onClick={() =>{setOpen(false)}} className="block">Home</NavLink>
+                <NavLink to="/product" onClick={() =>{setOpen(false)}}  className="block">All Product</NavLink>
+                {user &&
+                <NavLink to="/order" className="block">My Order</NavLink>
+                }
+                <NavLink to="/contact" className="block">Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
@@ -31,9 +40,13 @@ function Navbar() {
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+                {!user ? (<button onClick={()=>{setOpen(false);
+                    setShowUserLogin(true);
+                }} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
                     Login
-                </button>
+                </button>) : (<button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+                    Logout
+                </button>)}
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -46,14 +59,24 @@ function Navbar() {
             </button>
 
             {/* Mobile Menu */}
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
-                <NavLink to="#" className="block">Home</NavLink>
-                <NavLink to="#" className="block">About</NavLink>
-                <NavLink to="#" className="block">Contact</NavLink>
-                <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+            {open && (
+                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+                <NavLink to="/" onClick={() =>{setOpen(false)}} className="block">Home</NavLink>
+                <NavLink to="/product" onClick={() =>{setOpen(false)}}  className="block">All Product</NavLink>
+                {user &&
+                <NavLink to="/order" className="block">My Order</NavLink>
+                }
+                <NavLink to="/contact" className="block">Contact</NavLink>
+                {!user ? (<button onClick={()=>{setOpen(false);
+                    setShowUserLogin(true);
+                }} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
                     Login
-                </button>
+                </button>) : (<button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+                    Logout
+                </button>)}
             </div>
+            )
+            }
         </nav>  
         )
 }
